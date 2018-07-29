09908cd7c2b5c6b2a46fc3aa81fd99089957d426
Alexey Tchernopyatov <lexako1@mail.ru>
2018-07-30 00:17:05 +0500

    changed $index to item.id

--- assignment4/js/item-detail.controller.js
+++ assignment4/js/item-detail.controller.js
@@ -12,7 +12,14 @@ function ItemDetailController($stateParams,items) {
   var $ctrl = this;
   //var item = items[$stateParams.itemId];
   //var itemDetail = this;
-  var item = items.menu_items[$stateParams.itemId];
-  $ctrl.item = item;
+  for(var i = 0; i < items.menu_items.length; i++){
+    //console.log('items.menu_items[i].id = ', items.menu_items[i].id);
+    if(items.menu_items[i].id == $stateParams.itemId) {
+      $ctrl.item = items.menu_items[i];
+      break;
+    }
+  }
+  // var item = items.menu_items[$stateParams.itemId];
+  // $ctrl.item = item;
 }
 })();
